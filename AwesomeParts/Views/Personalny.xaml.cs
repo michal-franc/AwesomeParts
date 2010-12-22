195628d138e06e911d3fe0c500bf39d455043241
LaM <lam.michal.franc@gmail.com>
2010-12-22 21:45:03 +0100

    Added Redirect to the NoAcces Page if the user isn't in the Correct role for the view

--- AwesomeParts/Views/Personalny.xaml.cs
+++ AwesomeParts/Views/Personalny.xaml.cs
@@ -21,8 +21,32 @@ namespace AwesomeParts.Views
         public Personalny()
         {
             InitializeComponent();
-            //GrupujCB.SelectionChanged += GrupujCB_SelectionChanged;
-            PracownicyGrid.SelectionChanged += new SelectionChangedEventHandler(PracownicyGrid_SelectionChanged);
+            GrupujCB.SelectionChanged += GrupujCB_SelectionChanged;
+            PracownicyGrid.MouseWheel += new MouseWheelEventHandler(PracownicyGrid_MouseWheel);
+            //PracownicyGrid.SelectionChanged += new SelectionChangedEventHandler(PracownicyGrid_SelectionChanged);
+        }
+
+        void PracownicyGrid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                int rowsToMove = 0;
+                if (e.Delta < 0)
+                {
+                    rowsToMove = e.Delta / 120 * -1;
+                }
+                else
+                {
+                    rowsToMove = e.Delta / 120 * -1;
+                }
+
+                if (PracownicyGrid.SelectedIndex == 0
+                        || PracownicyGrid.SelectedIndex == (PracownicyGrid.ItemsSource.Cast<PracownikPOCO>().ToList().Count - 1))
+                { return; }
+
+                PracownicyGrid.SelectedIndex = PracownicyGrid.SelectedIndex + rowsToMove;
+                PracownicyGrid.ScrollIntoView(PracownicyGrid.SelectedItem, PracownicyGrid.Columns[0]);
+            }
         }
 
 
@@ -30,6 +54,10 @@ namespace AwesomeParts.Views
         // Executes when the user navigates to this page.
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            if (!WebContext.Current.User.IsInRole("DzialPersonalny"))
+            {
+                this.NavigationService.Navigate(new Uri("/NoAcces", UriKind.Relative));
+            }
         }
 
         private void GrupujCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
